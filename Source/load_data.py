19bbcad5913473b7ca403417d0f0600ff3022480
nickleu <nleuratti@gmail.com>
2026-01-20 06:18:26 +0100

    Generation data actual and forecast Wind & Solar

--- Source/load_data.py
+++ Source/load_data.py
@@ -10,3 +10,18 @@ def load_load_test():
     repo_root = Path(__file__).resolve().parent.parent
     csv_path = repo_root / "Data" / "raw" / "GUI_TOTAL_LOAD_DAYAHEAD_Test_24012025.csv"
     return pd.read_csv(csv_path)
+
+def load_onshore_wind_test():
+    repo_root = Path(__file__).resolve().parent.parent
+    csv_path = repo_root / "Data" / "raw" / "GUI_WIND_SOLAR_GENERATION_FORECAST_ONSHORE_Test_24012025.csv"
+    return pd.read_csv(csv_path)
+
+def load_offshore_wind_test():
+    repo_root = Path(__file__).resolve().parent.parent
+    csv_path = repo_root / "Data" / "raw" / "GUI_WIND_SOLAR_GENERATION_FORECAST_OFFSHORE_Test_24012025.csv"
+    return pd.read_csv(csv_path)
+
+def load_solar_test():
+    repo_root = Path(__file__).resolve().parent.parent
+    csv_path = repo_root / "Data" / "raw" / "GUI_WIND_SOLAR_GENERATION_FORECAST_SOLAR_Test_24012025.csv"
+    return pd.read_csv(csv_path)
